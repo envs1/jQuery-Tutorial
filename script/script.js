205cebecf7efcb67cf172e264e9b5cd77bbb5b42
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-15 14:49:16 +0000

    version 6

--- script/script.js
+++ script/script.js
@@ -1,5 +1,20 @@
 
-$("#lead-banner");
+//$("header nav li:even").css({border: "2px solid red"});
+//$("header nav li:last").css({border:"2px solid red"});
+/*
+$("#contact ul:first-child").css({border:"2px solid red"});
+$("#contact ul:last-child").css({border:"2px solid red"});*/
+
+/*$("section:not('#contact')").css({border:"2px solid green"});
+*/
+/*$("#social-nav li:lt(3)").css({border:"2px solid blue"});*/
+//$("#social-nav li:gt(2)").css({border:"2px solid blue "});
+
+//$("header nav li:odd").css({border:"2px solid yellow"});
+
+//$("section:not('#contact')").css({border:"2px solid green"});
+
+$("img[alt=quote]").css({border: "2px solid purple"});
 
 
 
@@ -29,6 +44,6 @@ $("#lead-banner");
 
 
 
- $("h3").css({border:"3px solid blue"});
+//$("h3").css({border:"3px solid blue"});
  
- $(".wrapper").css({border: "3px solid red"});
+// $(".wrapper").css({border: "3px solid red"});
